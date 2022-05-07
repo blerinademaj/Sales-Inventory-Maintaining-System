6c365f433183491ef46e8ddeaf09faf5a4bb0ccc
blerinademaj <95138459+blerinademaj@users.noreply.github.com>
2022-05-07 23:24:15 +0200

    Add files via upload

---
+++ Inventory.java
@@ -0,0 +1,12 @@
+package InventorySystem;
+import javax.swing.*;
+
+@SuppressWarnings("ALL")
+public interface Inventory {
+
+    JFrame f = new JFrame() ;
+
+    public static void generateJTable(){};
+    public boolean plotesohetKushti1();
+    public boolean plotesohetKushti2();
+}
